4c87d6b763b410e2b72268e27d3793563da36579
Neel Harip <nhari7@illinois.edu>
2026-07-18 23:24:00 -0400

    Add: house-robber-iii - submission-0

---
+++ Data Structures & Algorithms/house-robber-iii/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return [0, 0]
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+            withRoot = node.val + left[1] + right[1]
+            withoutRoot = max(left) + max(right)
+            return [withRoot, withoutRoot]
+
+        return max(dfs(root))
